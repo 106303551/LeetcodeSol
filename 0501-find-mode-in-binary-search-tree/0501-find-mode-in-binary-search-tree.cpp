cf10a9cf7f29aac1f5de3841c963483108bd0798
洪郡辰 <66413547+106303551@users.noreply.github.com>
2024-09-21 02:12:32 +0800

    Time: 16 ms (50.36%), Space: 22.8 MB (78.22%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -28,28 +28,22 @@ public:
         int maxcount = 1;
         InOrder(root,result);
         if(result.size()==1) return result;
-
+        //init
+        ans.push_back(result[0]);
         for(int i = 1;i<result.size();i++){
-            if(result[i]==result[i-1]){
+            if(result[i] == result[i-1]){
                 count++;
             }else{
-                if(maxcount == count){
-                    ans.push_back(result[i-1]);
-                }else if (maxcount<count){
-                    ans.clear();
-                    maxcount = count;
-                    ans.push_back(result[i-1]);
-                }
-                count = 1;
+                count =1;
             }
-        }
-        //考慮最後一個的情況
-        if(maxcount<count){
-            ans.clear();
-            ans.push_back(result[result.size()-1]);
-        }
-        if(count == maxcount){
-            ans.push_back(result[result.size()-1]);
+            if(count == maxcount){
+                ans.push_back(result[i]);    
+            }
+            if(count>maxcount){
+                ans.clear();
+                ans.push_back(result[i]);
+                maxcount = count;
+            }
         }
 
         return ans;
