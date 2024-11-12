9f5a2bbe1326ddea1ecc661a511fb19b6871dc7d
洪郡辰 <66413547+106303551@users.noreply.github.com>
2024-11-12 16:10:51 +0800

    Time: 0 ms (100%), Space: 8.2 MB (60.83%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+    vector<vector<int>> result;
+    vector<int> path;
+    int pathSum;
+    void backtracking(int k ,int n ,int startIndex){
+        
+        //決定加入result時間點
+        if(k-path.size() == 1){
+            if(n-pathSum>=startIndex && n-pathSum<=9){
+                path.push_back(n-pathSum);
+                result.push_back(path);
+                path.pop_back(); //撤掉最後一個node
+            }
+            return;
+        }
+        //for迴圈 檢查有無可能出現解之可能
+        
+        for(int i = startIndex;i<=9;i++){
+            path.push_back(i);
+            pathSum+=i;
+            backtracking(k,n,i+1);
+            path.pop_back();
+            pathSum-=i;
+        }
+    }
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        result.clear();
+        path.clear();
+        backtracking(k , n , 1);
+        return result;
+    }
+};
