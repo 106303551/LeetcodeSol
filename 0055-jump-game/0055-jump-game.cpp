80a2971685e39d859c239a300cc37d4aa7a68596
洪郡辰 <66413547+106303551@users.noreply.github.com>
2024-12-11 17:28:24 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -6,17 +6,15 @@ public:
         int farDes = 0;
         if(nums.size() == 1)return true;
 
-        while(Des<nums.size()-1){
-            if(nums[NowIndex]==0)return false;
-            for(int i =NowIndex;i<=Des;i++){
-                //更新最遠距離
-                if(Des<=i+nums[i]){
-                    Des = i+nums[i];
-                    NowIndex = i;
-                    if(Des>=nums.size())return true;
-                }
+        for(int i =NowIndex;i<=Des;i++){
+            //更新最遠距離
+            if(Des<=i+nums[i]){
+                Des = i+nums[i];
+                NowIndex = i;
+                if(Des>=nums.size())return true;
             }
         }
+        if(nums[NowIndex]==0)return false;
         return true;
     }
 };
