aea229b0b556fa9db546d6906c09475aef6a71c7
洪郡辰 <66413547+106303551@users.noreply.github.com>
2024-12-19 17:21:56 +0800

    Time: 59 ms (44.68%), Space: 94 MB (41.55%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -0,0 +1,38 @@
+class Solution {
+//思路：(1)確保上限由小到大 同上限的下限由大到小
+//     (2)相同上限最多保留一個 確認下一個上限是否overlap上一個下限
+public:
+    static bool cmp(vector<int>& a, vector<int>& b){
+        if(a[1] < b[1])return true;
+        if(a[1] == b[1] && a[0]>b[0])return true;
+        return false;
+    }
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int result = 0;
+        if(intervals.size() == 1)return result;
+        sort(intervals.begin(),intervals.end(),cmp);
+        int upperbound = intervals[intervals.size()-1][1];
+        int lowerbound = intervals[intervals.size()-1][0];
+        for(int i = intervals.size()-2; i>=0;i--){
+            //上限一樣
+            if(intervals[i][1] == upperbound){
+                lowerbound = intervals[i][0];
+                result++;
+                continue;
+            }
+            //下限與上限重疊
+            if(intervals[i][1]>lowerbound){
+                if(intervals[i][0]>lowerbound){
+                    upperbound = intervals[i][1];
+                    lowerbound = intervals[i][0];
+                }
+                result++;
+                continue;
+            }
+            //完全不重疊
+            upperbound = intervals[i][1];
+            lowerbound = intervals[i][0];
+        }
+        return result;
+    }
+};
