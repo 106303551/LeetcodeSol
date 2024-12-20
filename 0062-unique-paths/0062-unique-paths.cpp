8c93882d6c36783dbe4572adcacd9716dfa1edae
洪郡辰 <66413547+106303551@users.noreply.github.com>
2024-12-20 22:24:47 +0800

    Time: 0 ms (100%), Space: 9.4 MB (13.09%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        for(int i =1; i<m+1; i++){
+            for(int j = 1; j<n+1; j++){
+                dp[i][j] = max(dp[i-1][j]+dp[i][j-1],1);
+            }
+        }
+        return dp[m][n];
+    }
+};
